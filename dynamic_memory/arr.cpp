40043103ae7f631ff53b094274f637e1d390ad86
Vishal3239 <vishalpatelvp2004@gmail.com>
2026-04-20 09:16:16 +0530

    arr

---
+++ dynamic_memory/arr.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int *ptr = new int[5];
+    ptr[0]=1;
+    ptr[1]=2;
+    ptr[2]=3;
+    ptr[3]=4;
+    ptr[4]=5;
+    for(int i=0;i<5;++i){
+        cout << ptr[i]<<" ";
+    }
+    
+    return 0;
+}
